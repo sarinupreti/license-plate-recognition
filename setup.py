cdbe4851c6ae8cab78aaa7671b865fadb1f37a70
Sarin upreti <sarin@fawesomeapps.com>
2018-01-27 21:38:08 +0545

    license plate recognition

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AI_PROJECTS',
+    version='1',
+    packages=[''],
+    url='www.sarinupreti.com.np',
+    license='OPEN SOURCE',
+    author='sarin',
+    author_email='sarin.upreti01@gmail.com',
+    description='Number plate recognition system'
+)
